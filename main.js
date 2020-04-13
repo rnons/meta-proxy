8188972bfb75f6b0e309eecd81a796f0d515db1c
Ping Chen <remotenonsense@gmail.com>
2020-04-13 21:52:52 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const http = require("http");
+
+const fetch = require("node-fetch");
+
+const metascraper = require("metascraper")([
+  require("metascraper-description")(),
+  require("metascraper-image")(),
+  require("metascraper-logo")(),
+  require("metascraper-title")(),
+  require("metascraper-url")()
+]);
+
+const fetchMeta = async url => {
+  const res = await fetch(url);
+  const html = await res.text();
+  const metadata = await metascraper({ html, url });
+  return metadata;
+};
+
+const log = str => console.log(`${new Date().toISOString()} ${str}`);
+
+const server = http.createServer(async (req, res) => {
+  log(`GET ${req.url.slice(1)}`);
+  const meta = await fetchMeta(req.url.slice(1));
+  res.writeHeader(200, { "Content-Type": "application/json" });
+  res.write(JSON.stringify(meta));
+  res.end();
+});
+
+const port = 8000;
+
+server.listen(port, () => {
+  log(`Server started on ${port}`);
+});
