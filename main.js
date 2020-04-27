f805572e188f5ccf84ed09f9b764ed7ce1ea1cd7
Ping Chen <remotenonsense@gmail.com>
2020-04-27 18:03:36 +0900

    Cache meta of the latest 10000 urls

--- main.js
+++ main.js
@@ -10,22 +10,50 @@ const metascraper = require("metascraper")([
   require("metascraper-url")()
 ]);
 
+const MAX_STORE_SIZE = 10000;
+const store = new Map();
+
 const fetchMeta = async url => {
+  log(`fetching ${url}`);
   const res = await fetch(url);
   const html = await res.text();
   const metadata = await metascraper({ html, url });
   return metadata;
 };
 
+// Get meta from store or call fetchMeta
+const getMeta = async url => {
+  let meta = store.get(url);
+  if (meta) {
+    return meta;
+  }
+  meta = await fetchMeta(url);
+  store.set(url, meta);
+  release();
+  return meta;
+};
+
+// Limit store size to MAX_STORE_SIZE
+const release = () => {
+  if (store.size > MAX_STORE_SIZE) {
+    const iterator = store.keys();
+    for (let i = 0; i < MAX_STORE_SIZE / 20; i++) {
+      const key = iterator.next().value;
+      store.delete(key);
+    }
+  }
+};
+
 const log = str => console.log(`${new Date().toISOString()} ${str}`);
 
 const server = http.createServer(async (req, res) => {
-  log(`GET ${req.url.slice(1)}`);
-  const meta = await fetchMeta(req.url.slice(1));
+  const url = req.url.slice(1);
+  log(`GET ${url}`);
+  const meta = await getMeta(url);
   res.writeHeader(200, {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
-    "Cache-Control": "max-age=3600"
+    "Cache-Control": "max-age=86400"
   });
   res.write(JSON.stringify(meta));
   res.end();
